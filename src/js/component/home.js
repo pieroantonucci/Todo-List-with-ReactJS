0dc9ada04d7d04e39a92f8815adc50df0033dcf4
pieroantonucci <piero.antonucci@gmail.com>
2021-03-08 01:14:56 +0000

    Todo App with React

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,84 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrashAlt } from "@fortawesome/free-solid-svg-icons";
+import Button from "react-bootstrap/Button";
 
 //create your first component
-export function Home() {
+export function TodoApp() {
+	const [list, setList] = useState([]);
+	const [task, setTask] = useState({});
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+			<div className="title">
+				<h1>Todo List App</h1>
+			</div>
+			<div className="container">
+				<form
+					onSubmit={event => {
+						list.push(task);
+						setTask({ label: "" });
+						event.preventDefault();
+					}}>
+					<input
+						type="text"
+						value={task.label}
+						className="input"
+						onChange={event => {
+							setTask({ label: event.target.value, done: false });
+						}}
+						placeholder="Ingresa tu Tarea"
+					/>
+				</form>
+				<div className="task-list">
+					<hr></hr>
+					<ul className="list-group">
+						{list.length === 0
+							? "No hay tareas, ingresa tu primer To Do"
+							: ""}
+						{list.map((item, index) => {
+							return (
+								<li key={index} className="list-group-item">
+									{item.label}
+									<a>
+										<FontAwesomeIcon
+											icon={faTrashAlt}
+											color="white"
+											className="trashIcon"
+											onClick={() => {
+												setList(
+													list.filter(i => i !== item)
+												);
+											}}
+										/>
+									</a>
+								</li>
+							);
+						})}
+					</ul>
+				</div>
+				<hr></hr>
+				<div>
+					Delete All
+					<FontAwesomeIcon
+						icon={faTrashAlt}
+						color="white"
+						className="trashIcon"
+						onClick={() => {
+							setList([]);
+						}}
+					/>
+				</div>
+				<hr></hr>
+				<div>
+					<strong>
+						{list.length}{" "}
+						{list.length === 1
+							? "tarea pendiente"
+							: "tareas pendientes"}
+					</strong>
+				</div>
+			</div>
+		</>
 	);
 }
